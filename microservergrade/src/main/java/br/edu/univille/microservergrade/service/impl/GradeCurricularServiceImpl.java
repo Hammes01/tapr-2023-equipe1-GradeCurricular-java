0fb821b2a5544cd76458a6ee3ec9d37df01aef4e
Zédourado <103081912+Hammes01@users.noreply.github.com>
2023-11-24 01:11:56 +0000

    implementação do DAPR

--- microservergrade/src/main/java/br/edu/univille/microservergrade/service/impl/GradeCurricularServiceImpl.java
+++ microservergrade/src/main/java/br/edu/univille/microservergrade/service/impl/GradeCurricularServiceImpl.java
@@ -2,18 +2,28 @@ package br.edu.univille.microservergrade.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import br.edu.univille.microservergrade.entity.GradeCurricular;
 import br.edu.univille.microservergrade.repository.GradeCurricularRepository;
 import br.edu.univille.microservergrade.service.GradeCurricularService;
+
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
 
 @Service
 public class GradeCurricularServiceImpl implements GradeCurricularService {
 
    @Autowired
     private GradeCurricularRepository repository;
+    private DaprClient client = new DaprClientBuilder().build();
+    @Value("${app.component.topic.gradecurricular}")
+    private String TOPIC_NAME;
+    @Value("${app.component.service}")
+	private String PUBSUB_NAME;
    
-   
+
   
     @Override
     public List<GradeCurricular> getAll() {
@@ -36,7 +46,9 @@ public class GradeCurricularServiceImpl implements GradeCurricularService {
     @Override
     public GradeCurricular saveNew( GradeCurricular gradecurricular) {
             gradecurricular.setId(null);
-            return repository.save(gradecurricular);
+            gradecurricular = repository.save(gradecurricular);
+            publicarAtualizacao(gradecurricular);
+            return gradecurricular;
     }
 
     @Override
@@ -48,9 +60,10 @@ public class GradeCurricularServiceImpl implements GradeCurricularService {
 
             //Atualizar cada atributo do objeto antigo
             gradecurricularAntigo.setCurso(gradecurricular.getCurso());
+            gradecurricularAntigo = repository.save(gradecurricularAntigo);
+            publicarAtualizacao(gradecurricularAntigo);
+            return gradecurricularAntigo;
         
-
-            return repository.save(gradecurricularAntigo);
         }
         return null;
     }
@@ -66,5 +79,12 @@ public class GradeCurricularServiceImpl implements GradeCurricularService {
             return gradecurricular;
         }
         return null;
+    }
+        //método privado para publicar a atualização
+        private void publicarAtualizacao(GradeCurricular gradecurricular){
+            client.publishEvent(
+				    	PUBSUB_NAME,
+				    	TOPIC_NAME,
+				    	gradecurricular).block();
     }
 }
